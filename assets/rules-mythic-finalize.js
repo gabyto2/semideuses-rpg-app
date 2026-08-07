275d48d074f2ffae73247fe46a871b636e2032e2
gabyto2 <gabytosud@gmail.com>
2026-08-07 10:31:02 -0300

    finalizar catálogo mítico expandido

--- assets/rules-mythic-finalize.js
+++ assets/rules-mythic-finalize.js
@@ -1,1 +1,1 @@
-(function(g){'use strict';var d=g.SemideusesRulesDatabase;if(!d||!d.listConsumables||!d.listPanoplies)return;d.version='3e-rules-db-0.38.0';})(window);
+(function(g){if(g.SemideusesRulesDatabase)g.SemideusesRulesDatabase.version='3e-rules-db-0.40.0';})(window);
